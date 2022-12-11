776cc617e1a3400eefbc79fd6801dbe1586c072e
ilearn-code <satyamyadavv931@gmail.com>
2022-12-11 18:55:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-from flask import Flask,request,jsonify
-import numpy as np
+from flask import Flask, request, jsonify
 import pickle
+import numpy as np
 
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello world"
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
     cgpa = request.form.get('cgpa')
     iq = request.form.get('iq')
     profile_score = request.form.get('profile_score')
-
-    input_query = np.array([[cgpa,iq,profile_score]])
-
+    input_query = np.array([[cgpa, iq, profile_score]])
     result = model.predict(input_query)[0]
+    if result==1:
+        return jsonify('PLACED')
+    if result ==0:
+        return jsonify('NOT PLACED')
 
-    return jsonify({'placement':str(result)})
 
 if __name__ == '__main__':
     app.run(debug=True)
